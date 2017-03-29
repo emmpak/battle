5b9760bc25899039483ce070ee1f8eada55eafcf
Colin Turner <colinturner34@gmail.com>
2017-03-29 10:43:35 +0100

    Add failing test for Players form submission

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -5,3 +5,13 @@ feature "Testing infrastructure" do
     expect(page).to have_content('Testing infrastructure working!')
   end
 end
+
+feature "fill in names in form" do
+  scenario "checks that players can fill in their names on a form and submit that form" do
+    visit '/'
+    fill_in 'Player 1', :with => "Skeeter"
+    fill_in 'Player 2', :with => "Hutchkins"
+    click_button 'Submit'
+    expect(page).to have_content('BATTLE BEGINS!!!!')
+  end
+end
