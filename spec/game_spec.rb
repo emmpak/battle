570854ed0c6d926dbe437bc10dac9c652201e1b3
alicespyglass <alicespyglass@gmail.com>
2017-03-30 13:56:52 +0100

    start game class

---
+++ spec/game_spec.rb
@@ -0,0 +1,10 @@
+require 'game'
+
+describe Game do
+
+subject(:game) { described_class.new( player1:  player2: ) }
+let(:volkswagen) { double(:volkswagen) }
+let(:polar_bear) { double(:polar_bear) }
+
+
+end
